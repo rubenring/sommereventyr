80fd718851719d4bd8a2e1eb3a586e2f5dd6aaf0
RINGDAL Reuben <rringdal@one.steria.dom>
2018-06-17 15:57:20 +0200

    struktur og server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,25 @@ import { AppRouter } from './root/Router';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = () => {
+    const response =  fetch('/api/hello');
+    // const body =  response.json();
+
+    // if (response.status !== 200) throw Error(body.message);
+
+    return response;
+  };
   render() {
     return (
       <div className="App">
